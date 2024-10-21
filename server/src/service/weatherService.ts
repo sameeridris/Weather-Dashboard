2f8bb797f756d67fd650099cffffd81cad9040e5
Meez <meez@Meezs-MBP.lan>
2024-10-21 16:29:37 -0500

    initial commit

---
+++ server/src/service/weatherService.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// TODO: Define an interface for the Coordinates object
+
+// TODO: Define a class for the Weather object
+
+// TODO: Complete the WeatherService class
+class WeatherService {
+  // TODO: Define the baseURL, API key, and city name properties
+  // TODO: Create buildWeatherQuery method
+  // private buildWeatherQuery(coordinates: Coordinates): string {}
+  // TODO: Create fetchWeatherData method
+  // private async fetchWeatherData(coordinates: Coordinates) {}
+  // TODO: Build parseCurrentWeather method
+  // private parseCurrentWeather(response: any) {}
+  // TODO: Complete buildForecastArray method
+  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  // TODO: Complete getWeatherForCity method
+  // async getWeatherForCity(city: string) {}
+}
+
+export default new WeatherService();
