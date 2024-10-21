0f33a1265a04a9fd7fc0387409a0770c876b5f33
Meez <meez@Meezs-MBP.lan>
2024-10-21 16:53:45 -0500

    built the weatherService file

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,15 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  lat: string,
+  lon: string,
+}
 
-// TODO: Define a class for the Weather object
-
-// TODO: Complete the WeatherService class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
-  // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
-  // TODO: Create fetchWeatherData method
   private async fetchForecastData(coordinates: Coordinates) {
     let forecastData= await fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${process.env.API_KEY}`);
     let weatherJSON= await forecastData.json()
@@ -18,14 +15,23 @@ class WeatherService {
   }
   // TODO: Build parseCurrentWeather method
   private async fetchCurrentWeather(coordinates: Coordinates) {
-    let WeatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${process.env.API_KEY}`)
+    let WeatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${process.env.API_KEY}`);
     let weatherJSON= await WeatherData.json()
     return weatherJSON;
   }
   // TODO: Complete buildForecastArray method
-  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: any, weatherData: any[]) {
+    return [...currentWeather, ...weatherData];
+  }
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+    let locationData = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.API_Key}`);
+    let locationJSON = await locationData.json();
+    let forecastData= await this.fetchForecastData(locationJSON[0]);
+    let currentWeather= await this.fetchCurrentWeather(locationJSON[0]);
+    let forecastArray= this.buildForecastArray(currentWeather, forecastData);
+    return forecastArray;
+  }
 }
 
 export default new WeatherService();
