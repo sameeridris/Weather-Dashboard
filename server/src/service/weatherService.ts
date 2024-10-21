3e9dc058a85879eadbb21cba19d08aa6a08d851e
Meez <meez@Meezs-MBP.lan>
2024-10-21 16:36:48 -0500

    added the fetch CurrentWeather and ForcastData to the weatherService file

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -11,11 +11,19 @@ class WeatherService {
   // TODO: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchForecastData(coordinates: Coordinates) {
+    let forecastData= await fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${process.env.API_KEY}`);
+    let weatherJSON= await forecastData.json()
+    return weatherJSON.list;
+  }
   // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
+  private async fetchCurrentWeather(coordinates: Coordinates) {
+    let WeatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${process.env.API_KEY}`)
+    let weatherJSON= await WeatherData.json()
+    return weatherJSON;
+  }
   // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
   // async getWeatherForCity(city: string) {}
 }
